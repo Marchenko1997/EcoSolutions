1bfe9e352f10bd1bd1cfbfb847e8d09747c41eba
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-01-10 13:45:27 +0100

    updating styles for Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,33 @@
 import Logo from "./Logo/Logo";
+import { HeaderContainer, HeaderMain, ButtonsBox } from "./Header.styled";
+import {useState, useEffect} from 'react'
 
 const Header = () => {
-    return <div>
-    <Logo />
-  </div>;
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const isScrolled = window.scrollY > 50;
+        if (isScrolled !== scrolled) {
+          setScrolled(!scrolled);
+        }
+      };
+
+      document.addEventListener("scroll", handleScroll, { passive: true });
+
+      return () => {
+        document.removeEventListener("scroll", handleScroll);
+      };
+    }, [scrolled]);
+    return (
+      <>
+        <HeaderMain data-scrolled={scrolled}>
+          <HeaderContainer>
+            <Logo />
+          </HeaderContainer>
+        </HeaderMain>
+      </>
+    );
 };
 
 export default Header;
---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const HeaderMain = styled.header`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  padding: 36px 0;
+  background-color: ${({ "data-scrolled": scrolled }) =>
+    scrolled ? "var(--white-color)" : "var(--main-bg)"};
+  transition: background-color 0.3s ease;
+  z-index: 100;
+
+  @media screen and (min-width: 1280px) {
+    padding: 24px 0;
+  }
+`;
+
+export const HeaderContainer = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (min-width: 768px) {
+    max-width: 768px;
+    padding: 0 30px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    max-width: 1280px;
+    padding: 0 100px;
+  }
+`;
+
+export const ButtonsBox = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 12px;
+
+  a {
+    display: none;
+
+    @media screen and (min-width: 768px) {
+      display: flex;
+      width: 140px;
+    }
+  }
+`;
