be4d9cbc7cba6ec110ded2701e6e3b975519f13b
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-01-17 21:06:26 +0100

    update MainValues style section

--- src/components/MainValues/MainValues.jsx
+++ src/components/MainValues/MainValues.jsx
@@ -1,9 +1,35 @@
-import TitleValues from "./TitleValues/TitleValues"
+import TitleValues from "./TitleValues/TitleValues";
+import TextValues from "./TextValues/TextValues";
+import { Section, TextContainer, TitleContainer } from "./MainValues.styled";
 
 const MainValues = () => {
+  const variants = {
+    hidden: { opacity: 0, y: 150 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.7,
+        delay: 0.2,
+      },
+    },
+  };
   return (
-    <div><TitleValues/></div>
-  )
-}
+    <Section
+      id="about"
+      variants={variants}
+      initial="hidden"
+      whileInView="show"
+      viewpoit={{ once: true }}
+    >
+      <TitleContainer>
+        <TitleValues />
+        <TextContainer>
+          <TextValues />
+        </TextContainer>
+      </TitleContainer>
+    </Section>
+  );
+};
 
-export default MainValues
+export default MainValues;
---
+++ src/components/MainValues/MainValues.styled.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const Section = styled(motion.section)`
+  padding-bottom: 36px;
+
+  @media screen and (min-width: 768px) {
+    padding-bottom: 100px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding-bottom: 120px;
+  }
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 24px;
+  margin-bottom: 36px;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+    margin-bottom: 100px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    margin-bottom: 122px;
+  }
+`;
+
+export const TextContainer = styled.div`
+  @media screen and (min-width: 768px) {
+    width: 353px;
+    padding-left: 11px;
+    border-left: 1px solid var(--accent-color);
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 559px;
+    padding-left: 100px;
+  }
+`;
