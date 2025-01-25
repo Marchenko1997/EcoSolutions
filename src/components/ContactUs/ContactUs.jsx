2e7dcefb8c6c9f4c70ffd3b4320590ff4fbfcb34
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-01-25 21:14:05 +0100

    integrating meta tags

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,12 +1,14 @@
 import ContactTitle from "./ContactTitle/ContactTitle"
 import { Section, FlexContainer } from "./ContactUs.styled"
 import ContactForm from "./ContactForm/ContactForm"
+import ContactInfo from "./ContactInfo/ContactInfo"
 
 const ContactUs = () => {
   return (
       <Section>
           <ContactTitle />
           <FlexContainer>
+              <ContactInfo />
               <ContactForm />
           </FlexContainer>
     </Section>
